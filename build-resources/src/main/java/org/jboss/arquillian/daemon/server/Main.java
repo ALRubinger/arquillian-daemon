d5721ffb56fb8c67677e99dc3813fc6d1a4b39da
Andrew Lee Rubinger <alr@alrubinger.com>
2012-10-24 00:19:28 -0700

    [ARQ-1169] Create build

---
+++ build-resources/src/main/java/org/jboss/arquillian/daemon/server/Main.java
@@ -0,0 +1,10 @@
+package org.jboss.arquillian.daemon.server;
+
+public class Main
+{
+
+   public static void main(String[] args)
+   {
+      System.out.println("Hi there! I was forged as part of the project you call server.");
+   }
+}
